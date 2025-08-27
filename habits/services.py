55c2bd4513d113ac3203883a5c00a846ecca1ff6
AlTim <aleks00_777@mail.ru>
2025-08-27 17:49:59 +0300

    update app habits

--- habits/services.py
+++ habits/services.py
@@ -2,9 +2,9 @@ import requests
 from config.settings import TELEGRAM_BOT_TOKEN
 
 
-def send_telegram_message(message, chat_id):
+def send_telegram_message(message, tg_chat):
     """
     Отправка сообщения в TG
     """
-    params = {"text": message, "chat_id": chat_id}
+    params = {"text": message, "chat_id": tg_chat}
     requests.get(f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage", params=params)
--- habits/tasks.py
+++ habits/tasks.py
@@ -8,7 +8,7 @@ from habits.services import send_telegram_message
 @shared_task
 def send_habit():
     habits = Habit.objects.all()
-    current_date = datetime.datetime.now()
+    current_date = datetime.datetime.now().time()
     for habit in habits:
         if habit.time <= current_date:
             tg_chat = habit.user.tg_chat_id
